52f69eed246a7512584aa260ec27d578b4526e2e
ranjangaurav555 <gauravranjan875@gmail.com>
2026-09-26 13:49:44 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    "signup_login_db",
-    "root",
-    "gaurav@05",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
         dialect: "mysql",
         logging: false
     }
