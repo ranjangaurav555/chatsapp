52e752edeae0af4d9f40df43c76db761fb1ee23d
ranjangaurav555 <gauravranjan875@gmail.com>
2026-09-26 15:36:58 +0530

    Add files via upload

---
+++ GroupMember.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const GroupMember = sequelize.define("GroupMember", {
+
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    groupId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+
+}, {
+
+    tableName: "groupMembers",
+
+    timestamps: true
+
+});
+
+module.exports = GroupMember;
